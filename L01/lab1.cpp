e5c5d21a7c0511121b1edc6bc2d73abedf5e9304
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2025-04-03 19:16:16 -0300

    Create lab1.cpp
    Reconoce certamenes válidos
    Falta implementar las funciones para hacer el código más legible
    Vector de corruptos eventualemente se elimina

---
+++ L01/lab1.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fstream>
+using namespace std;
+
+string readFile(ifstream &f){
+    string text, content;
+    while (getline (f, text)){
+        content += text + "\n";
+    }
+    return content;
+}
+
+bool valid_type(string t){ 
+    //CAMBIAR PARA QUE RETORNE CERTAMEN 0 CONTROL 1 TAREA 2
+    return (t == "certamen" || t == "control" || t == "tarea");
+}
+
+bool valid_num(string n){
+    int num = stoi(n);
+    return (num >= 1 && num <= 3);
+}
+
+bool valid_year(string y){
+    if (y.length() < 6) return false;
+    
+    string year = y.substr(0, 4);
+    string sem = string(1, y[5]);
+    
+    int year_ = stoi(year);
+    int sem_ = stoi(sem);
+
+    return (year_ <= 2025 && sem_ >= 1 && sem_ <= 2);
+}
+
+// CAMBIAR POR UN DO WHILE
+
+
+void leer_dir(){
+    
+    DIR *dir1;
+    struct dirent *entrada;
+    vector<string> corruptos;
+
+    dir1 = opendir("/home/isidora/OneDrive/Escritorio/uni/so/Lab1/Pruebas/Pruebas/General"); // CAMBIAR POR LA DIRECCION CORRESPONDIENTE
+
+    if (dir1 == NULL){
+        cout << "Error abriendo el directorio." << endl;
+        return;
+    }
+
+    while ((entrada = readdir(dir1)) != NULL){
+        string file_name = entrada->d_name;
+        if (file_name == "." || file_name == ".."){
+            continue;
+        }
+        string f = "/home/isidora/OneDrive/Escritorio/uni/so/Lab1/Pruebas/Pruebas/General/" + file_name;
+        ifstream file(f);
+        if (file.is_open()){
+            string texto = readFile(file);
+            size_t pos = texto.find("tipo: ");
+            if (pos != string::npos){ //SI SE ENCUENTRA TIPO
+                pos += 6;
+                string type = texto.substr(pos, texto.find("\n", pos) - pos); //DESDE POS A SALTO DE LINEA
+                type.erase(remove(type.begin(), type.end(), '\n'), type.end());
+                cout << type << endl;
+                if (type == "certamen" || type == "certamen\n" || type == "certamen\r"){
+                    pos = texto.find("numero: ");
+                    if (pos != string::npos){ //SI SE ENCUENTRA NUMERO
+                        pos += 8;
+                        int num = stoi(texto.substr(pos, texto.find("\n", pos) - pos));
+                        if (num > 3 || num < 0){
+                            corruptos.push_back(file_name); //INVALIDO
+                        } else {
+                            pos = texto.find("semestre_publicacion: ");
+                            if (pos != string::npos){ //SI ES QUE SE ENCUENTRA SEMESTRE
+                                pos += 22;
+                                string anio = texto.substr(pos, 4);
+                                int sem = stoi(texto.substr(pos + 5, 1));
+                                if (stoi(anio) > 2025 || sem < 1 || sem > 2){ //INVAlIDO
+                                    corruptos.push_back(file_name);
+                                } else {
+                                    cout << "SLAI" << endl; //EXITO ^V^ 
+                                    cout << type << " " << num << " " << anio << " " << sem << endl; // PROBANDO :P :P :P
+                                }
+                            } else {
+                                corruptos.push_back(file_name);
+                            } 
+                        }                                                                                                    
+                    } else {
+                        corruptos.push_back(file_name);
+                    }
+                } else {
+                cout<<" "<<endl;
+                }
+            } else {
+                corruptos.push_back(file_name);
+            }
+            file.close();
+        } else {
+            cout << "No se pudo abrir :C" << endl;
+        }
+    }
+
+    if (closedir(dir1) == -1){
+        cout << "Error cerrando el directorio." << endl;
+        return;
+    }
+
+}
+
+int main(){
+    leer_dir();
+    return 0;
+}

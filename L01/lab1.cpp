117990d6896d9d5e277d7084a6e2085e87a785ce
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2025-04-03 21:58:27 -0300

    Update lab1.cpp
    Reconoce certamenes, controles y tareas correctos. Dejé los cout para que se entendiera mejor como se filtraba

--- L01/lab1.cpp
+++ L01/lab1.cpp
@@ -14,31 +14,31 @@ string readFile(ifstream &f){
     return content;
 }
 
-bool valid_type(string t){ 
-    //CAMBIAR PARA QUE RETORNE CERTAMEN 0 CONTROL 1 TAREA 2
-    return (t == "certamen" || t == "control" || t == "tarea");
-}
-
 bool valid_num(string n){
     int num = stoi(n);
     return (num >= 1 && num <= 3);
 }
 
-bool valid_year(string y){
-    if (y.length() < 6) return false;
-    
-    string year = y.substr(0, 4);
-    string sem = string(1, y[5]);
-    
-    int year_ = stoi(year);
-    int sem_ = stoi(sem);
-
-    return (year_ <= 2025 && sem_ >= 1 && sem_ <= 2);
+bool valid_year(string y, int n){
+    int y_ = stoi(y);
+    if (y_ < 2026 && n >= 1 && n <= 2){
+        return true;
+    }
+    return false;
 }
 
+bool valid_hw(string name){
+    vector<string> Tareas = {"onda certera", "laser vs. plasma", "quantumanio", "atomoX", "bitfase", "red-aracnida", "debug & dragons", "thread wars", "crypto knight", "bugzilla", "stack attack", "compile & conquer", "algorhythm", "cyberkarma"};
+    transform(name.begin(), name.end(), name.begin(), ::tolower);
+    for (int i = 0; i < Tareas.size(); i++){
+        if (name == Tareas[i] || name == Tareas[i]+"\r" || name == Tareas[i]+"\n"){
+            return true;
+        }
+    }
+    return false;
+}
 
 void leer_dir(){
-    
     DIR *dir1;
     struct dirent *entrada;
     vector<string> corruptos;
@@ -79,48 +79,68 @@ void leer_dir(){
                                 if (stoi(anio) > 2025 || sem < 1 || sem > 2){ //INVAlIDO
                                     corruptos.push_back(file_name);
                                 } else {
-                                    cout << "Certamen valido" << endl; //EXITO ^V^ 
+                                    cout << "Certamen valido: "; //EXITO ^V^ 
+                                    cout << sem << " " << anio << " " << num << endl;
                                 }
                             } else {
-                                corruptos.push_back(file_name);
+                                cout << "NO" << endl; // INVALIDO
                             } 
                         }                                                                                                    
                     } else {
-                        corruptos.push_back(file_name);
+                        cout << "NO" << endl; // INVALIDO
                     }
-                } else {
-                    if (type == "control" || type == "control\n" || type == "control\r"){
-                        pos = texto.find("numero: ");
-                        if (pos != string::npos){ // SI ES QUE SE ENCUENTRA NUMERO
-                            pos += 8;
-                            string numero = texto.substr(pos, texto.find("\n", pos));
-                            if (valid_num(numero)){
-                                pos = texto.find("semestre_publicacion: ");
-                                if (pos != string::npos){ // SI SE ENCUENTRA EL SEMENSTRE
-                                    pos += 22;
-                                    string publicacion = texto.substr(pos, texto.find("\n", pos));
-                                    if (valid_year(publicacion)){
-                                        cout << "Control valido" << endl;
-                                    } else {
-                                        //INVALIDO
-                                        cout <<"NO"<<endl;
-                                    }
+                } else if (type == "control" || type == "control\n" || type == "control\r"){
+                    pos = texto.find("numero: ");
+                    if (pos != string::npos){ // SI ES QUE SE ENCUENTRA NUMERO
+                        pos += 8;
+                        string numero = texto.substr(pos, texto.find("\n", pos) - pos);
+                        if (valid_num(numero)){
+                            pos = texto.find("semestre_publicacion: ");
+                            if (pos != string::npos){ // SI SE ENCUENTRA EL SEMESTRE
+                                pos += 22;
+                                string anio = texto.substr(pos, 4);
+                                int sem = stoi(texto.substr(pos + 5, 1));
+                                if (valid_year(anio, sem)){
+                                    cout << "Control valido: ";
+                                    cout << anio << " " << sem << " " << numero << endl;
                                 } else {
-                                    //SEMESTRE INVALIDO
-                                    cout <<"NO"<<endl;
+                                    cout << "NO" << endl; //INVALIDO
                                 }
                             } else {
-                                //numero invalido
-                                cout <<"NO"<<endl;
+                                cout << "NO" << endl; //SEMESTRE INVALIDO
                             }
-                        }
-                    } else {
-                        if (type == "tarea" || type == "tarea\n" || type == "tarea\r"){
-                            cout << "HW" << endl;
                         } else {
-                            cout << "lolaso No" << endl;
+                            cout << "NO" << endl; //NUMERO INVALIDO
                         }
                     }
+                } else if (type == "tarea" || type == "tarea\n" || type == "tarea\r"){
+                    pos = texto.find("semestre_publicacion: ");
+                    if (pos != string::npos){
+                        pos += 22;
+                        string anio = texto.substr(pos, 4);
+                        int sem = stoi(texto.substr(pos + 5, 1));
+                        if (valid_year(anio, sem)){
+                            size_t pos2 = texto.find("nombre: ");
+                            if (pos2 != string::npos){
+                                pos2 += 8;
+                                string nombre = texto.substr(pos2, texto.find("\n", pos2) - pos2);
+                                if (valid_hw(nombre)){
+                                    cout << "Tarea valida: ";
+                                    cout << sem << " " << anio << " " << nombre << endl;
+                                } else {
+                                    cout << "NO" << endl; //INVALIDO
+                                }
+                            } else {
+                                cout << "NO" << endl; //INVALIDO
+                            }
+                        } else {
+                            cout << "NO" << endl; // INVALIDO
+                        }
+                    } else {
+                        cout << "NO" << endl; // INVALIDO
+                    }
+                } else {
+                    cout << "lolaso No" << endl;
                 }
             } else {
                 corruptos.push_back(file_name);
@@ -135,7 +155,6 @@ void leer_dir(){
         cout << "Error cerrando el directorio." << endl;
         return;
     }
-
 }
 
 int main(){
